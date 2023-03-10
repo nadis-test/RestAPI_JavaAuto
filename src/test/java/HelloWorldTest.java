ec451b660823b100e7c927c818927fa14a53ec5a
nadezhda.porotkova <nadezhda.porotkova@karuna.group>
2023-03-10 20:58:53 +0100

    parse json example

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,14 +1,31 @@
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
     @Test
-    public void testHelloWorld(){
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                .andReturn();
-        System.out.println(response.htmlPath().getNode("body").toString());
+    public void testRestAssured(){
+        Map<String, String> params = new HashMap<>(); //объявляем переменную для хранения payload - набора параметров и значений для нашего запроса
+        params.put("name","John"); //заполняем хэшмэпу значениями
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String name = response.get("answer2");
+        if (name == null ) {
+            System.out.println("The key 'answer2' is absent");
+        }
+        else {
+            System.out.println(name);
+        }
+        //System.out.println(response.htmlPath().getNode("body").toString());
+        //response.prettyPrint();
     }
 
 }
