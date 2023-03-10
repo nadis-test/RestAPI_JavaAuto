a4fec77e209abde3dde7a302a396aea737712006
nadezhda.porotkova <nadezhda.porotkova@karuna.group>
2023-03-10 21:55:50 +0100

    add and read headers

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,3 +1,4 @@
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
@@ -9,17 +10,25 @@ import java.util.Map;
 public class HelloWorldTest {
     @Test
     public void testRestAssured(){
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Header1","Value1");
+        headers.put("Header2","Value2");
 
         Response response = RestAssured
                 .given()
                 .redirects()
                 .follow(false)
+                .headers(headers)
                 .when()
                 .get("https://playground.learnqa.ru/api/get_303")
                 .andReturn();
-        int statusCode = response.statusCode();
-        System.out.println(statusCode);
-        response.print();
+
+        response.prettyPrint();
+
+        Headers responseHeaders = response.getHeaders();
+        System.out.println(responseHeaders);
+        String locationHeader = response.getHeader("location");
+        System.out.println("Location header = " + locationHeader);
     }
 
 }
