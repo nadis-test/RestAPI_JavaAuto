95580ec9aa49ad15e4d57133b855612da4a34d29
nadezhda.porotkova <nadezhda.porotkova@karuna.group>
2023-03-11 12:30:42 +0100

    hw get result from API after waiting

--- src/test/java/HomeworkTests.java
+++ src/test/java/HomeworkTests.java
@@ -4,8 +4,6 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 public class HomeworkTests {
     @Test
     public void parseJSON(){
@@ -55,4 +53,66 @@ public class HomeworkTests {
 
         System.out.println("Final redirects number: " + (redirects_number-1));
     }
+
+    @Test
+    public void checkLongJobStatus() throws InterruptedException {
+        String status_ready = "Job is ready";
+        String status_not_ready = "Job is NOT ready";
+        String error = "";
+        String status = status_not_ready;
+        int time = 0;
+
+        // 1 заводим задачу
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+
+        //получаем токен
+        String token = response.getString("token");
+        System.out.println("Token value: " + token);
+        //получаем время джобы
+        time = response.getInt("seconds");
+        System.out.println("Time value: " + time);
+
+            while ((status.compareTo(status_not_ready) == 0)) {
+                //выполняем запрос с токеном
+                JsonPath response1 = RestAssured
+                        .given()
+                        .queryParam("token", token)
+                        .when()
+                        .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                        .jsonPath();
+                //извлекаем ошибку из ответа
+                error = response1.getString("error");
+                //если оишбки нет
+                if (error == null) {
+                    // печатаем ответ
+                    System.out.println("\nResponse print");
+                    response1.prettyPrint();
+                    // узнаем статус
+                    status = response1.getString("status");
+                    System.out.println("\nJob status: " + status);
+                    // если job is not ready, то запускаем ожидание
+                    if (status.compareTo(status_not_ready) == 0){
+                        // запускаем ожидание
+                        System.out.println("Time wait millisec: " + (time)*1000);
+                        Thread.sleep((time)*1000);
+                    }
+                    // если job is ready, то проверяем наличие результата и печатаем его
+                    else if (status.compareTo(status_ready) == 0){
+                        String result = response1.getString("result");
+                        if (result != null) {
+                            System.out.println("\nJob result: " + result);
+                        }
+                        else{
+                            System.out.println("\nError: 'result' is missing in response");
+                        }
+                    }
+                }
+                else {
+                    System.out.println("\nError: " + error);
+                }
+
+            }
+        }
 }
