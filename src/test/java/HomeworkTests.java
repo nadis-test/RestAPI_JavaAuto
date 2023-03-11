39d06518f7b4ab2ceebefb98de74549bda1cf13c
nadezhda.porotkova <nadezhda.porotkova@karuna.group>
2023-03-11 10:41:19 +0100

    hw print follow several redirects

--- src/test/java/HomeworkTests.java
+++ src/test/java/HomeworkTests.java
@@ -1,5 +1,7 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -20,6 +22,36 @@ public class HomeworkTests {
         }
         else {
             System.out.println("\nsecond message does not exist");
+        }
+    }
+
+    @Test
+    public  void printAllRedirects(){
+        int redirects_number = 0;
+        int statusCode = 0;
+        String url = "https://playground.learnqa.ru/api/long_redirect";
+        String location;
+        while (statusCode != 200) {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(url)
+                    .andReturn();
+
+            Headers headers = response.getHeaders();
+            System.out.println("\nHeaders print on iteration = " + redirects_number);
+            System.out.println(headers);
+
+            statusCode = response.getStatusCode();
+            System.out.println("\nStatusCode: " + statusCode);
+
+            url = response.getHeader("location");
+            System.out.println("Redirect address (location): " + url);
+
+            redirects_number++;
+
         }
     }
 }
