fc33f9aad2f32aa39bc0134e366555fcb39ec978
nadezhda.porotkova <nadezhda.porotkova@karuna.group>
2023-03-13 11:38:51 +0100

    auth negativee test and usage of RequestSpecification

--- src/test/java/UserAuthTest.java
+++ src/test/java/UserAuthTest.java
@@ -1,7 +1,11 @@
 import io.restassured.RestAssured;
 import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 import java.util.HashMap;
@@ -55,4 +59,54 @@ public class UserAuthTest {
         //сравниваем id при проверке и при авторизации, если они равны - то юзер авторизован
         assertEquals(userAuthOnId, userIdOnCheck, "'user_id' on check doesn't match with 'user_id' on auth");
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"cookie", "headers"})
+    public void testNegativeAuthUser(String condition){
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        // выполняем запрос на авторизацию
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .when()
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        //из ответа вынимаем cookie
+        Map<String, String> cookies = responseGetAuth.cookies();
+        //из ответа вынимаем headers
+        Headers headers = responseGetAuth.getHeaders();
+
+        //создаем сущность RequestSpecification, чтобы сконструировать запрос с нужными параметрами на выбор из ValueSource
+        RequestSpecification spec = RestAssured.given();
+        //засовываем в сущность RequestSpecification адрес эндпойнта
+        spec.baseUri("https://playground.learnqa.ru/api/user/auth");
+
+        //засовываем в сущность RequestSpecification те параметры, которые указаны в ValueSource
+        if (condition.equals("cookie")) {  //если в condition из ValueSource пришло "cookie"
+            spec.cookie("auth_sid", cookies.get("auth_sid")); //то в spec мы передаем куку с названием 'auth_sid' и значением
+            //которое мы вытащили из response: cookies = responseGetAuth.cookies()
+        }
+        else if (condition.equals("headers")) {  //если в condition из ValueSource пришло "header"
+                spec.header("x-csrf-token", headers.get("x-csrf-token")); //то в spec мы передаем хэдер с названием 'x-csrf-token'
+                                                                    // и значением из headers = responseGetAuth.getHeaders();
+            }
+        else {
+            //это на случай, если мы неправильно вдруг параметры указали и ни одно узловие if не выполнилось
+            throw new IllegalArgumentException("Condition value is known: " + condition);
+        }
+
+        // делаем запрос со сконструированными параметрами и получаем его ответ в виде JsonPath
+        JsonPath responseForCheck = spec.get().jsonPath();
+
+        //проверяем значение user_id - т.к. наш тест негативный и мы намеренно  передавали в запрос только куку ИЛИ только хэдер
+        // то юзер не должен быть авторизован (условие авторизации = И хэдер, И кука переданы)
+        // неавторизованный юзер имеер user_id = 0 по требованиям в нашем API
+        assertEquals(0, responseForCheck.getInt("user_id"), "'user_id' should be 0 for unauth user");
+    }
+
 }
+
