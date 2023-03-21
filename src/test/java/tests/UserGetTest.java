e72c7c8db3fac82bb5b002f82da01be3b85d2d65
nadezhda.porotkova <nadezhda.porotkova@karuna.group>
2023-03-21 12:48:05 +0100

    hw Ex16 add test to get user dara with other user id

--- src/test/java/tests/UserGetTest.java
+++ src/test/java/tests/UserGetTest.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static lib.AssertionsCustom.*;
+import static lib.DataGenerator.getGenerationData;
 
 public class UserGetTest extends BaseTestcase {
     private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
@@ -54,5 +55,37 @@ public class UserGetTest extends BaseTestcase {
         System.out.println(responseUserData.asString());
         String[] fields = {"username", "email", "firstName", "lastName"};
         assertJsonHasFields(responseUserData, fields);
+    }
+
+    @Test
+    @Description("This test tries to get user data without necessary parameters: token and auth-cookie")
+    @DisplayName("Test negative: get user data without token and auth-cookie")
+    public void testGetUserDataWithOtherUserId() {
+        //авторизуемся существующем пользователем и получаем токен и куку
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", "vinkotov@example.com");
+        userData.put("password", "1234");
+
+        Response responseGetAuth = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/login", userData);
+
+        String cookie = this.getCookie(responseGetAuth, "auth_sid"); // вызываем getCookie из lib/BaseTestcase
+        String header = this.getHeader(responseGetAuth,"x-csrf-token"); // вызываем getHeader из lib/BaseTestcase
+        
+        //создаем нового пользователя, чтобы получить существующий id
+        userData = getGenerationData();
+        Response responseCreateNewAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+        int user_id_new = responseCreateNewAuth.jsonPath().getInt("id");
+
+        //делаем запрос на эндпойнт авторизации с кукой и токеном первого юзера и с id второго юзера
+        Response responseUserData = apiCoreRequests.makeGetRequestWithTokenAndCookie(
+                "https://playground.learnqa.ru/api/user/" + user_id_new,
+                header,
+                cookie);
+
+        assertJsonHasField(responseUserData, "username");
+        assertJsonHasNotField(responseUserData, "email");
+        assertJsonHasNotField(responseUserData, "firstName");
+        assertJsonHasNotField(responseUserData, "lastName");
     }
 }
